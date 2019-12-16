fec2fe1894f20a4373c099ff196d7be24782a43f
cur_ <cur_@rezxis.net>
2019-12-16 17:17:41 +0900

    docker

--- src/net/rezxis/mchosting/bungee/Bungee.java
+++ src/net/rezxis/mchosting/bungee/Bungee.java
@@ -9,6 +9,7 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.connection.Server;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.event.PreLoginEvent;
 import net.md_5.bungee.api.event.ServerKickEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
