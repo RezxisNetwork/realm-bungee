e7bb013aa657f2f747b662b472585e42c95c077a
cur-114 <cursan0409@gmail.com>
2020-08-16 11:56:26 +0900

    fix

--- src/main/java/net/rezxis/mchosting/bungee/tasks/RewardTask.java
+++ src/main/java/net/rezxis/mchosting/bungee/tasks/RewardTask.java
@@ -8,6 +8,7 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.rezxis.mchosting.database.Tables;
 import net.rezxis.mchosting.database.crates.CrateTypes;
 import net.rezxis.mchosting.database.object.player.DBPlayer;
+import net.rezxis.mchosting.database.object.player.DBVote;
 
 public class RewardTask implements Runnable {
 
@@ -17,11 +18,19 @@ public class RewardTask implements Runnable {
 			if (player.getServer().getInfo().getName().equalsIgnoreCase("lobby"))
 				continue;
 			int coin = 50;
+			int box = 1;
 			DBPlayer dp = Tables.getPTable().get(player.getUniqueId());
+			DBVote dv = Tables.getVTable().getVoteByUUID(player.getUniqueId());
+			if (dv != null) {
+				if (dv.hasRank()) {
+					coin *=2;
+					box *=2;
+				}
+			}
 			player.sendMessage(ChatColor.AQUA+"おつかれさまでした! "+ChatColor.LIGHT_PURPLE+coin+"枚"+ChatColor.AQUA+"のコインが手に入った");
-			player.sendMessage(ChatColor.GREEN+"報酬箱を手に入れました！ロビーでチェストをクリックして、開けます！");
+			player.sendMessage(ChatColor.GREEN+String.valueOf(box)+"報酬箱を手に入れました！ロビーでチェストをクリックして、開けます！");
 			dp.addCoin(coin);
-			dp.setVault(dp.getVault()+1);
+			dp.setVault(dp.getVault()+box);
 			dp.update();
 			if (new Random().nextInt(100)<= 80) {
 				Tables.getCTable().giveCrate(player.getUniqueId(), CrateTypes.NORMAL);
