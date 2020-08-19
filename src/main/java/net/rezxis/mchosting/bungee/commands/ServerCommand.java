457280b00d1dfd8399a14f100a2ff430e6e3c8e6
cur-114 <cursan0409@gmail.com>
2020-08-19 19:46:00 +0900

    fix

--- src/main/java/net/rezxis/mchosting/bungee/commands/ServerCommand.java
+++ src/main/java/net/rezxis/mchosting/bungee/commands/ServerCommand.java
@@ -51,15 +51,17 @@ public class ServerCommand extends Command implements TabExecutor {
 			texts.add(text);
 			Iterator<ServerWrapper> ite = list.iterator();
 			ServerWrapper s = ite.next();
-			TextComponent z = new TextComponent(s.getDisplayName()+ChatColor.GREEN+"("+String.valueOf(s.getPlayers())+")");
-			texts.add(z);
 			TextComponent connect = new TextComponent("クリックで接続");
 			connect.setColor(ChatColor.AQUA);
 			connect.setBold(true);
+			TextComponent z = new TextComponent(s.getDisplayName()+ChatColor.GREEN+"("+String.valueOf(s.getPlayers())+")");
+			z.setClickEvent(new ClickEvent(Action.RUN_COMMAND, "server "+ChatColor.stripColor(s.getDisplayName())));
+			z.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponent[] {connect}));
+			texts.add(z);
 			while (ite.hasNext()) {
 				ServerWrapper ts = ite.next();
 				TextComponent tx = new TextComponent(","+ts.getDisplayName()+ChatColor.RESET+""+ChatColor.GREEN+"("+String.valueOf(ts.getPlayers())+")");
-				tx.setClickEvent(new ClickEvent(Action.RUN_COMMAND, "/server "+ChatColor.stripColor(ts.getDisplayName())));
+				tx.setClickEvent(new ClickEvent(Action.RUN_COMMAND, "server "+ChatColor.stripColor(ts.getDisplayName())));
 				tx.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponent[] {connect}));
 				texts.add(tx);
 			}
